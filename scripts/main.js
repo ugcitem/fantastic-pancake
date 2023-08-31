4b889fe9e1536a61e35da834e57c583cd5e9de6b
ugcitem <138194312+ugcitem@users.noreply.github.com>
2023-08-31 09:09:58 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
     const embedButton = document.getElementById("embedButton");
-    const embeddedIframe = document.getElementById("embeddedIframe");
 
     embedButton.addEventListener("click", function() {
         const websiteInput = document.getElementById("websiteInput").value;
-        embeddedIframe.src = websiteInput;
+        openEmbeddedWebsite(websiteInput);
     });
 });
+
+function openEmbeddedWebsite(url) {
+    const newWindow = window.open('about:blank', '_blank');
+    
+    if (newWindow) {
+        newWindow.document.write('<iframe src="' + url + '" frameborder="0" style="width: 100%; height: 100vh;"></iframe>');
+        newWindow.document.close();
+    } else {
+        alert('Pop-up blocked. Please allow pop-ups to view the embedded website.');
+    }
+}
